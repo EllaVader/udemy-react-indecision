40bd39394fffb419921751be09fefd6b13e537d4
EllaVader <jroupon@yahoo.com>
2017-12-18 14:18:05 -0500

    adding onForm submit and remove all button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,42 +5,92 @@ console.log('App.js is running');
 var app = {
   title: 'Indecision App',
   subTitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 };
 
-// JSX - Javascript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    'p',
-    null,
-    app.subTitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
-  ),
-  React.createElement(
-    'ol',
+var render = function render() {
+  // JSX - Javascript XML
+  var template = React.createElement(
+    'div',
     null,
     React.createElement(
-      'li',
+      'h1',
       null,
-      'Item one'
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      'p',
+      null,
+      app.subTitle
     ),
     React.createElement(
-      'li',
+      'p',
       null,
-      'Item two'
+      app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: onRemoveAll },
+      'Clear All Options'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item two'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
     )
-  )
-);
+  );
+  //will only render those elements that need to be updated
+  ReactDOM.render(template, appRoot);
+};
+
+//onSubmit takes the event as an argument
+var onFormSubmit = function onFormSubmit(event) {
+  //stop the full page refresh
+  event.preventDefault();
+  //get the value the user typed
+  //event.target points the element that the event started on, for our example its' the form element.
+  //you have access to all of the elements on the form  option.value is the "name" of the element and value is it's value
+  var option = event.target.elements.option.value;
+
+  //check to see if a value was typed in
+  if (option) {
+    //add this option to the array
+    app.options.push(option);
+    //clear out the text box
+    event.target.elements.option.value = '';
+    render();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
 
 var appRoot = document.getElementById('app');
+
+render();
--- src/app.js
+++ src/app.js
@@ -3,20 +3,56 @@ console.log('App.js is running');
 const app = {
   title: 'Indecision App',
   subTitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 };
 
-// JSX - Javascript XML
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
+const render = () => {
+  // JSX - Javascript XML
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Clear All Options</button>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  //will only render those elements that need to be updated
+  ReactDOM.render(template, appRoot);
+};
+
+//onSubmit takes the event as an argument
+const onFormSubmit = (event) => {
+  //stop the full page refresh
+  event.preventDefault();
+  //get the value the user typed
+  //event.target points the element that the event started on, for our example its' the form element.
+  //you have access to all of the elements on the form  option.value is the "name" of the element and value is it's value
+  const option = event.target.elements.option.value
+
+  //check to see if a value was typed in
+  if(option){
+    //add this option to the array
+    app.options.push(option);
+    //clear out the text box
+    event.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+}
 
 const appRoot = document.getElementById('app');
+
+render();
