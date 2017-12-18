73ab6f66616ea45e18ffe66a844dee7d0650505b
EllaVader <jroupon@yahoo.com>
2017-12-18 16:51:10 -0500

    Examples of extending classes and overriding parent class methods

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -9,16 +9,50 @@ class Person {
   }
 
   getDescription() {
-    return `${this.name} is ${this.age} old`;
+    return `${this.name} is ${this.age} old.`;
   }
 
 }
 
-const me = new Person('Janine Roe', 49);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age); //call the parent constructor function
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major
+  }
+
+  //override the parent's method
+  getDescription() {
+    let description = super.getDescription();  //call the parent's getDescription
+
+    if (this.hasMajor()){
+      description += ` Their is major is ${this.major}`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  
+  constructor(name, age, homeLocation){
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting(){
+    let greeting = super.getGreeting();
+    if(this.homeLocation){
+      greeting += ` I'm visiting from ${this.homeLocation}`
+    }
+    return greeting;
+  }
+}
+
+const me = new Traveler('Janine Roe', 49, 'Glenmoore');
 console.log(me.getGreeting());
-console.log(me.getDescription());
 
-
-const other = new Person();
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
-console.log(other.getDescription());
