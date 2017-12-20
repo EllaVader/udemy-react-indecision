aed50d4faf287f00e22d99ab8c4041dfd5748cdd
EllaVader <jroupon@yahoo.com>
2017-12-20 10:44:37 -0500

    Example on how to handle Component State using setState

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,31 +1,87 @@
+class Counter extends React.Component {
+ 
+  constructor(props){
+   super(props);
+   this.handleAddOne = this.handleAddOne.bind(this);
+   this.handleMinusOne = this.handleMinusOne.bind(this);
+   this.handleReset = this.handleReset.bind(this);
 
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
+   // track the following state default values
+   this.state = {
+    count: 0
+   };
+ }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      };
+    });
+  }
+  handleAddOne() {
+    // to change the state of the count property, you need to call setState
+    // setState takes a method as an argument.  It returns an object back
+    // setState has one argument, which is the objects previous state
+    // only update the properties on the object that we care about, we can ignore the others
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
 
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-  //will only render those elements that need to be updated
-  ReactDOM.render(templateTwo, appRoot);
-};
 
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+
+// let count = 0;
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
+
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// };
+
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
+
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
+//   //will only render those elements that need to be updated
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
