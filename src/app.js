9a5200cd56a477c270b65b3a900348b840ae0479
EllaVader <jroupon@yahoo.com>
2017-12-22 17:09:28 -0500

    Adding babel-loader to webpack and necessary config files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-//import './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 25));
-
-console.log(isAdult(5));
-console.log(canDrink(21));
-console.log(isSenior(65));
+const template = <p>This is JSX from webpack</p>
+ReactDOM.render(template, document.getElementById('app'));
