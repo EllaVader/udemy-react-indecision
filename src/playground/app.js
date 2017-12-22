564d783266a4f7abed8645bde87c3d91815a5d1f
EllaVader <jroupon@yahoo.com>
2017-12-22 15:46:10 -0500

    Adding webpack and configuration for webpack

---
+++ src/playground/app.js
@@ -0,0 +1,211 @@
+class IndecisionApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    //need to bind so the child component can have a proper reference to
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+
+    //we want to track the state of the options as they get updated
+    this.state = {
+      options: []
+    };
+  }
+
+  // lifecycle method on React that is called at the beginning of the life-cycle
+  componentDidMount() {
+
+    try {
+      //if parsing throws an exception, catch it and do nothing
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      //if options is not empty, then load it.
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+
+    } catch (e) {
+      // do nothing at all
+    }
+
+  }
+  // lifecyle method on React when a component updates / changes
+  componentDidUpdate(prevProps, prevState) {
+    //save our options to local storage
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  // lifecycle method on React when a component when a new component is loaded (i.e. navigating to a new page)
+  componentWillUnmount() {
+    console.log('Component will unmount');
+  }
+  handlePick() {
+    //pick a random element in the array
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
+
+  // handleDeleteOptions method needed here at parent level
+  // but a child component will call it.
+  handleDeleteOptions() {
+    //short cut way to call set state - wrap object in parenthesis
+    this.setState(() => ({ options: [] }))
+  }
+
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => option !== optionToRemove)
+    }))
+  }
+
+  handleAddOption(option) {
+    //error check
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+
+    // Use set state to update the state of the property -- it will automatically re-render as it see's it's been updated
+    // we don't want to manipulate the state or prevState object directly, so set the options prop to a new array
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }));
+  }
+
+  render() {
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    );
+  }
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+// creating default properties
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+const Action = (props) => {
+  return (
+    <div>
+      <button
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What should I do?
+      </button>
+    </div>
+  );
+}
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
+      {
+        props.options.map(option => (
+          <Option
+            key={option}
+            optionText={option}
+            handleDeleteOption={props.handleDeleteOption}
+          />
+        ))
+      }
+    </div>
+  );
+};
+
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+      <button
+        onClick={(e) => {
+          // use the onclick event that takes the event as an argument,
+          // now call parent handle.deleteOption with the text we want to delete.
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        remove
+      </button>
+    </div>
+  )
+};
+
+class AddOption extends React.Component {
+
+  // we need a constructor for this class because we are using "this" below in the handleAddOption event handler method
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+
+    // keeping track of the error message
+    this.state = {
+      error: undefined
+    };
+  }
+
+  // we have a handleAddOption here (check if option is valued and clear the form)
+  // but we also call the parent's handleAddOption because it owns the state of the options array
+  handleAddOption(e) {
+
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    // call the parent's method
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      // clear out the text box
+      e.target.elements.option.value = '';
+    }
+
+  }
+  render() {
+    // onSubmit call this classes handleAddOption, which calls the parent class handleAddOption
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// define entry point - path to our main driver file
+// define output of final bundle file - path to where the public version of the file will live.
+
+//__dirname is property gives you the absolute path to the current module (file)
+//use node path to build the path to where we want to put the output file
+
+// module.exports a way to expose an object to another file via node
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
