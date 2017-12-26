24822107e06c670be7b16c4f2109a64902500d22
EllaVader <jroupon@yahoo.com>
2017-12-26 12:26:56 -0500

    adding button styling and modifiers

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
   <div>
     {props.optionText}
     <button
+      className="button button--link"
       onClick={(e) => {
         // use the onclick event that takes the event as an argument,
         // now call parent handle.deleteOption with the text we want to delete.
