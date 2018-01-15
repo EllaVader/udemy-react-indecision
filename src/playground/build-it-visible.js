ef664f85b0102f52be1b2c42b6438a945ba1f58d
Janine Roe <EllaVader@users.noreply.github.com>
2018-01-15 11:06:41 -0500

    removed commented out code

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -32,29 +32,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-
-// let visibility = false;
-// const render = () => {
-  
-//   const template = (
-//     <div>
-//       <h1>Visibility Toggle</h1>
-//       <button onClick={toggleMe}>{ visibility ? 'Hide Details' : 'Show Details' }</button>
-      // { visibility && (
-      //   <div>
-      //     <p>Hey you wanted to see this!</p>
-      //   </div>
-      // )}
-//     </div>
-//   );
-
-//   ReactDOM.render(template, document.getElementById('app'));
-// };
-
-// const toggleMe = () => {
-//   visibility = !visibility;
-//   render();
-// }
-
-// render();
